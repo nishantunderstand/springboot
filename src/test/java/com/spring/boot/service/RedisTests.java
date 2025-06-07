e51494ce74ae3ec0359239c902bfb939c0585ea5
NishantRaj <nishant.read@gmail.com>
2025-06-07 18:52:54 +0530

    LenovoBat 07-Jun-25 18:52:53.76

--- src/test/java/com/spring/boot/service/RedisTests.java
+++ src/test/java/com/spring/boot/service/RedisTests.java
@@ -4,17 +4,23 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class RedisTests {
-	
+
 	@Autowired
-	private RedisTemplate redisTemplate;
-	
+	private RedisTemplate<String, Object> redisTemplate;
+
 	@Test
-	void testSendEmail() {
-		redisTemplate.opsForValue().set("email", "gmail@gmai.com");
-		Object email = redisTemplate.opsForValue().get("email");
-		System.out.println("=== Working====");
+	public void testGetUserFromRedis() {
+		// Get the value for key "user"
+		Object userValue = redisTemplate.opsForValue().get("user");
+
+		// Print the value
+		System.out.println("Value from Redis for key 'user': " + userValue);
+
+		// Assert that the value is not null (optional)
+		assertNotNull(userValue, "Value for key 'user' should not be null");
 	}
 }
